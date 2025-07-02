e8df714b7b2658d9c40326de7c10def3ed14750a
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-07-02 10:03:29 +0300

    post_course_form_part_2 => worked on making the file component resuble for differents user inputs

---
+++ apps/client/src/common/components/forms/inputs/FieldFile.tsx
@@ -0,0 +1,108 @@
+/** @jsxImportSource @emotion/react */
+"use client";
+
+import { FormFieldType } from "@/common/types/uiFactory";
+import { ForwardedRef, forwardRef, useImperativeHandle, useRef } from "react";
+import {
+  Control,
+  Controller,
+  ControllerRenderProps,
+  FieldErrors,
+  FieldValues,
+  UseFormRegister,
+} from "react-hook-form";
+import Anchor from "../etc/Anchor";
+import ErrFormField from "../errors/ErrFormField";
+import { JSX } from "react/jsx-runtime";
+import { css } from "@emotion/react";
+
+type PropsType<T extends FieldValues> = {
+  el: FormFieldType<T>;
+  control: Control<T>;
+  register: UseFormRegister<T>;
+  errors: FieldErrors<T>;
+  isData: boolean;
+  Preview: React.ReactElement;
+  onChange: ({
+    e,
+    field,
+  }: {
+    e: React.ChangeEvent<HTMLInputElement>;
+    field: ControllerRenderProps<T>;
+  }) => void;
+  accept: string;
+  multiple: boolean;
+};
+
+const FieldFileInner = <T extends FieldValues>(
+  {
+    control,
+    el,
+    register,
+    errors,
+    isData,
+    Preview,
+    onChange,
+    accept,
+    multiple,
+  }: PropsType<T>,
+  ref: ForwardedRef<HTMLInputElement | null>,
+) => {
+  const localRef = useRef<HTMLInputElement>(null);
+
+  // ? force bind outer ref to inner ref
+  useImperativeHandle(ref, () => localRef.current as HTMLInputElement, []);
+
+  return (
+    <label htmlFor={el.name} className="w-full grid grid-cols-1">
+      <span className="txt__lg text-neutral-200">{el.label}</span>
+
+      <Controller
+        name={el.name}
+        control={control}
+        render={({ field }) => (
+          <input
+            name={el.name}
+            ref={(elHTML) => {
+              field.ref(elHTML);
+
+              localRef.current = elHTML;
+            }}
+            multiple={multiple}
+            required={el.required}
+            type="file"
+            accept={accept}
+            className="w-0 h-0 opacity-0"
+            onChange={(e) => {
+              onChange({ e, field });
+            }}
+          />
+        )}
+      />
+
+      <div className="w-full relative max-w-fit">
+        {Preview}
+
+        <ErrFormField
+          {...{
+            el,
+            errors,
+            $customCSS: {
+              css: css`
+                min-width: 350px;
+                ${isData ? "right:5%" : "left:5%"};
+              `,
+            },
+          }}
+        />
+        <Anchor {...{ name: el.name, register }} />
+      </div>
+    </label>
+  );
+};
+
+const FieldFile = forwardRef(FieldFileInner) as <T extends FieldValues>(
+  props: PropsType<T> & { ref: ForwardedRef<HTMLInputElement | null> },
+) => JSX.Element;
+
+export default FieldFile;
--- apps/client/src/common/components/forms/inputs/FormFieldVideo/FormFieldVideo.tsx
+++ apps/client/src/common/components/forms/inputs/FormFieldVideo/FormFieldVideo.tsx
@@ -2,14 +2,16 @@
 "use client";
 
 import { BtnActType, FormFieldType } from "@/common/types/uiFactory";
-import { Controller, FieldValues, useFormContext } from "react-hook-form";
-import ErrFormField from "../../errors/ErrFormField";
+import {
+  ControllerRenderProps,
+  FieldValues,
+  useFormContext,
+} from "react-hook-form";
 import { isStr } from "@shared/first/lib/dataStructure";
-import { css } from "@emotion/react";
-import Anchor from "../../etc/Anchor";
 import BtnShadow from "../../../buttons/BtnShadow/BtnShadow";
 import { useRef } from "react";
 import PreviewVideo from "./components/PreviewVideo";
+import FieldFile from "../FieldFile";
 
 type PropsType<T extends FieldValues> = {
   el: FormFieldType<T>;
@@ -34,56 +36,38 @@ const FormFieldVideo = <T extends FieldValues>({ el }: PropsType<T>) => {
     if (inputRef.current) inputRef.current.click();
   };
 
+  const handleChange = ({
+    e,
+    field,
+  }: {
+    e: React.ChangeEvent<HTMLInputElement>;
+    field: ControllerRenderProps<T>;
+  }) => {
+    const {
+      target: { files },
+    } = e;
+
+    field.onChange(files?.[0] ?? null);
+  };
+
   return (
     <div className="w-full">
-      <label htmlFor={el.name} className="w-full grid grid-cols-1">
-        <span className="txt__lg text-neutral-200">{el.label}</span>
+      <FieldFile
+        {...{
+          accept: "video/*",
+          control,
+          el,
+          errors,
+          isData,
+          multiple: false,
+          onChange: handleChange,
+          Preview: <PreviewVideo {...{ vid: vid }} />,
+          register,
+        }}
+        ref={inputRef}
+      />
 
-        <Controller
-          name={el.name}
-          control={control}
-          render={({ field }) => (
-            <input
-              name={el.name}
-              ref={(elHTML) => {
-                field.ref(elHTML);
-                inputRef.current = elHTML;
-              }}
-              required={el.required}
-              type="file"
-              accept="video/*"
-              className="w-0 h-0 opacity-0"
-              onChange={(e) => {
-                const {
-                  target: { files },
-                } = e;
-
-                field.onChange(files?.[0] ?? null);
-              }}
-            />
-          )}
-        />
-
-        <div className="w-full relative max-w-fit">
-          <PreviewVideo {...{ vid: vid }} />
-
-          <ErrFormField
-            {...{
-              el,
-              errors,
-              $customCSS: {
-                css: css`
-                  min-width: 350px;
-                  ${isData ? "right:5%" : "left:5%"};
-                `,
-              },
-            }}
-          />
-          <Anchor {...{ name: el.name, register }} />
-        </div>
-      </label>
-
-      <div className="w-full max-w-1/2 mt-4">
+      <div className="w-full max-w-[600px] mt-4">
         <div className="w-full max-w-[250px]">
           <BtnShadow
             {...{
