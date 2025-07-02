5ed300280d7372e106109996c69e0d5e14fe4927
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-07-02 07:43:37 +0300

    post_course_form_part_2 => text to make vercel ignore  uild

--- vercel-ignore-build-step.ts
+++ vercel-ignore-build-step.ts
@@ -1,6 +1,4 @@
 import { execSync } from "child_process";
-import { existsSync, mkdirSync, writeFileSync } from "fs";
-import { join } from "path";
 import { env, exit } from "process";
 
 const branch = env.VERCEL_GIT_COMMIT_REF || env.GIT_BRANCH;
@@ -8,33 +6,8 @@ const branch = env.VERCEL_GIT_COMMIT_REF || env.GIT_BRANCH;
 if (branch !== "main") {
   console.log(`⛔ Skipping build for branch: ${branch}`);
 
-  const nextDir = join(process.cwd(), "apps/client/.next");
-
-  if (!existsSync(nextDir)) mkdirSync(nextDir, { recursive: true });
-
-  const manifestPath = join(nextDir, "routes-manifest.json");
-
-  if (!existsSync(manifestPath)) {
-    writeFileSync(
-      manifestPath,
-      JSON.stringify(
-        {
-          version: 3,
-          pages404: true,
-          dynamicRoutes: [],
-          rewrites: [],
-        },
-        null,
-        2,
-      ),
-    );
-    console.log("✅ Injected stub routes-manifest.json");
-  }
-
   exit(0);
 }
 
 console.log(`✅ Proceeding with build for branch: ${branch}`);
 execSync("turbo build --filter=client...", { stdio: "inherit" });
-
-// a
