424028b2a37f3a140004ff634cd262d890950ce1
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-07-04 08:53:26 +0300

    api_setup => understanding a little better and translating express syntax to fastify one

---
+++ apps/server/src/middleware/log.ts
@@ -0,0 +1,18 @@
+import { __cg } from "@shared/first/lib/logger.js";
+import { FastifyReply, FastifyRequest } from "fastify";
+import { app_dir } from "src/lib/system/index.js";
+import fs from "fs";
+import path from "path";
+
+const logPath = path.resolve(app_dir, "logger", "data.json");
+const logDir = path.dirname(logPath);
+
+export const logJSON = async (req: FastifyRequest, _: FastifyReply) => {
+  if (!fs.existsSync(logDir))
+    await fs.promises.mkdir(logDir, { recursive: true });
+
+  if (!fs.existsSync(logPath))
+    await fs.promises.writeFile(logPath, JSON.stringify([]));
+
+  return;
+};
--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -3,28 +3,22 @@ import Fastify from "fastify";
 import router from "./routes/index.js";
 import decorators from "./decorators/index.js";
 import db from "./conf/db.js";
-import { doStuff, doStuffB } from "./dev_only/index.js";
 import { __cg } from "@shared/first/lib/logger.js";
-import { sum } from "@shared/first/lib/index.js";
 import corsPlugin from "./middleware/cors.js";
 
 const app = Fastify({
   logger: {
-    transport: {
-      target: "pino-pretty",
-      options: {
-        colorize: true,
-        translateTime: "HH:MM:ss Z",
-        ignore: "pid,hostname",
-      },
-    },
+    level: "warn",
+    // transport: {
+    //   target: "pino-pretty",
+    //   options: {
+    //     colorize: true,
+    //     translateTime: "HH:MM:ss Z",
+    //     ignore: "pid,hostname",
+    //   },
+    // },
   },
 });
-
-sum();
-
-// doStuff();
-// doStuffB();
 
 const start = async () => {
   try {
@@ -39,7 +33,7 @@ const start = async () => {
 
     await app.listen({ port: app.env.PORT, host: app.env.HOST });
 
-    __cg("[DEBUG] Starting Fastify on", app.env);
+    __cg(`=> server running on ${app.env.PORT}`);
   } catch (err) {
     await db.$disconnect();
     app.log.error(err);
