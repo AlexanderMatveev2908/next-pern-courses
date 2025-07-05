8fb46fad927ac89eeae5b605c7e5d75579d5d899
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-07-05 08:04:09 +0300

    test => created issue

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -2,7 +2,7 @@ import env from "./conf/env.js";
 import Fastify from "fastify";
 import router from "./routes/index.js";
 import db from "./conf/db.js";
-import { __cg } from "@shared/first/lib/logger.js";
+import { __cg  from "@shared/first/lib/logger.js";
 import { DEL_ALL } from "./dev_only/danger.js";
 
 const app = Fastify({
