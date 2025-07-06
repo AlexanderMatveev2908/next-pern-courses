2328c6cb03b0c2c8b026d35f84eccb260b293cd7
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-07-06 13:36:58 +0300

    template_courses_page => made util to write read sql

--- apps/server/src/features/courses/controllers/get.ts
+++ apps/server/src/features/courses/controllers/get.ts
@@ -1,8 +1,16 @@
+import { __cg } from "@shared/first/lib/logger.js";
+import db from "@src/conf/db.js";
+import { readSQL } from "@src/lib/system/index.js";
 import { FastifyReply, FastifyRequest } from "fastify";
 
 export const getListCoursesCtrl = async (
   req: FastifyRequest,
   res: FastifyReply,
 ) => {
-  return res.res200({});
+  __cg("server count", readSQL("get_courses"));
+  const courses = await db.$queryRawUnsafe(readSQL("get_courses"));
+
+  return res.res200({
+    courses,
+  });
 };
