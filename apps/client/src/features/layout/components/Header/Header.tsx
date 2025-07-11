075a4c08e218824909567721bece55a7afede5c2
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-07-11 10:05:13 +0300

    ui_course_page => try disable watchers vsc, having high level lug and delay on save

--- apps/client/src/features/layout/components/Header/Header.tsx
+++ apps/client/src/features/layout/components/Header/Header.tsx
@@ -8,6 +8,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { getSideState, sideSlice } from "../Sidebar/slice";
 import { resp } from "@/core/lib/style";
 import { css } from "@emotion/react";
+import { useListenHydration } from "@/core/hooks/api/useListenHydration";
 
 const Header: FC = () => {
   const sideState = useSelector(getSideState);
@@ -15,6 +16,8 @@ const Header: FC = () => {
   const dispatch = useDispatch();
   const handleClick = (val: boolean) =>
     dispatch(sideSlice.actions.setSide(val));
+
+  const { isHydrated } = useListenHydration();
 
   return (
     <div className="pl-[10px] sm:pl-[20px] pr-[10px] w-full border-b-[3px] border-b-neutral-800 sticky top-0 h-[80px] z__header bg-[#000]">
@@ -29,6 +32,7 @@ const Header: FC = () => {
             aria-label="toggle sidebar"
             className="btn__app w-fit justify-self-end"
             onClick={handleClick.bind(null, !sideState.isOpen)}
+            disabled={!isHydrated}
             style={
               {
                 "--scale__up": 1.3,
