3a399df4a54cca69531c4e308b10ccd2279a60cb
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-07-14 15:11:14 +0300

    ui_concept_page => imp

--- apps/server/src/services/grabAssetsItem.ts
+++ apps/server/src/services/grabAssetsItem.ts
@@ -3,7 +3,7 @@ import { EntityType } from "@prisma/client";
 
 export const sqlStrImages = (
   entity: EntityType,
-  { prefix }: { prefix: string },
+  { prefix }: { prefix: "c" | "cpt" },
 ) =>
   sql([
     `
@@ -32,18 +32,7 @@ export const grabAssetsItem = (
 ) =>
   sql([
     `
-  (
-    SELECT json_agg(
-      json_build_object(
-        'url', ca."url",
-        'publicID', ca."publicID"
-      )
-    )
-    FROM "CloudAsset" AS ca
-    WHERE ca."type" = 'IMAGE'
-      AND ca."entityID" = ${prefix}."id"
-      AND ca."entityType" = '${entity}'::"EntityType"
-  ) AS "images",
+  ${sqlStrImages(entity, { prefix }).text} AS "images",
 
   (
     SELECT json_build_object(
