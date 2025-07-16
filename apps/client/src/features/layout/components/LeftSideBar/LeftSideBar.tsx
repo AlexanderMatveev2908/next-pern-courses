becab969902302d04cd4324f22925dd1810c8865
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-07-16 17:31:02 +0300

    template_left_search_bar => defined 2 cols and diveder for sidebar

--- apps/client/src/features/layout/components/LeftSideBar/LeftSideBar.tsx
+++ apps/client/src/features/layout/components/LeftSideBar/LeftSideBar.tsx
@@ -31,7 +31,7 @@ const LeftSideBar: FC = () => {
 
       <motion.div
         ref={sideRef}
-        className="z__left_side fixed top-[80px] left-0 h-full w-[80%] md:w-[400px] bg-[#000] border-r-[3px] border-neutral-800 -translate-x-full grid grid-cols-1"
+        className="z__left_side fixed top-[80px] left-0 h-full w-[80%] md:w-[400px] bg-[#000] border-r-[3px] border-neutral-800 -translate-x-full"
         transition={{ duration: 0.3, ease: easeInOut }}
         animate={{
           opacity: leftSideState.isSide ? 1 : 0.5,
@@ -41,12 +41,22 @@ const LeftSideBar: FC = () => {
           pointer-events: ${leftSideState.isSide ? "all" : "none"};
         `}
       >
-        <div className="w-[100px] relative h-fit justify-self-end mr-1 pt-10">
-          <CloseBtn
-            {...{
-              handleClick: () => dispatch(leftSideSLice.actions.setSide(false)),
-            }}
-          />
+        <div className="grid grid-rows-[45px_1fr] min-h-0 h-full">
+          <div className="w-[100px] relative h-fit max-h-fit justify-self-end mr-1 pt-10">
+            <CloseBtn
+              {...{
+                handleClick: () =>
+                  dispatch(leftSideSLice.actions.setSide(false)),
+              }}
+            />
+          </div>
+
+          <div className="w-full grid grid-cols-[1fr_3px_2fr] min-h-full h-full">
+            <div className=" mt-2"></div>
+
+            <div className="min-h-full w-full bg-neutral-800 min-w-full"></div>
+            <div className=" mb-2"></div>
+          </div>
         </div>
       </motion.div>
     </>
