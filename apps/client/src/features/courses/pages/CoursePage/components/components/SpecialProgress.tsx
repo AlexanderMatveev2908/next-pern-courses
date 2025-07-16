96803cc0eb528155f66c743ef7f52e439cad393d
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-07-16 10:26:28 +0300

    special_custom_progress_bar => generated width filler clors based perc accomplishemt concepts quiz done

--- apps/client/src/features/courses/pages/CoursePage/components/components/SpecialProgress.tsx
+++ apps/client/src/features/courses/pages/CoursePage/components/components/SpecialProgress.tsx
@@ -4,6 +4,8 @@
 import { CourseType } from "@/features/courses/types/courses";
 import { __cg } from "@shared/first/lib/logger.js";
 import type { FC } from "react";
+import { css } from "@emotion/react";
+import { useGenIDsV2 } from "@/core/hooks/ui/useGenIDsV2";
 
 type PropsType = {
   course: CourseType;
@@ -13,10 +15,28 @@ const SpecialProgress: FC<PropsType> = ({ course }) => {
   const { concepts = [] } = course;
   const completedCount = concepts.filter((cpt) => cpt.isCompleted).length;
   const perc = (completedCount / concepts.length) * 100;
-  __cg("perc", perc);
+
+  const {
+    ids: [innerIDs],
+  } = useGenIDsV2({
+    lengths: [concepts.length],
+  });
+
+  __cg("paragon", innerIDs);
+
+  __cg("cpts", concepts);
 
   return (
-    <div className="w-full border-[3px] border-neutral-800 h-[50px] rounded-full"></div>
+    <div className="w-full border-[3px] border-neutral-800 h-[50px] rounded-full relative">
+      <div className="absolute inset-0 overflow-hidden rounded-full">
+        <div
+          className="absolute top-0 left-0 h-full bg-neutral-200"
+          css={css`
+            width: ${perc}%;
+          `}
+        ></div>
+      </div>
+    </div>
   );
 };
 
