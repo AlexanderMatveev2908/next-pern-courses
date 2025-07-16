f9c2fac6a117efadd976adcd8c773edb8a7b1fff
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-07-16 10:49:19 +0300

    special_custom_progress_bar => used grid for prgress bar cause flex did not work for this version as i wanted

--- apps/client/src/features/courses/pages/CoursePage/components/components/SpecialProgress.tsx
+++ apps/client/src/features/courses/pages/CoursePage/components/components/SpecialProgress.tsx
@@ -6,6 +6,7 @@ import { __cg } from "@shared/first/lib/logger.js";
 import type { FC } from "react";
 import { css } from "@emotion/react";
 import { useGenIDsV2 } from "@/core/hooks/ui/useGenIDsV2";
+import ImgLoader from "@/common/components/HOC/assets/ImgLoader";
 
 type PropsType = {
   course: CourseType;
@@ -16,18 +17,41 @@ const SpecialProgress: FC<PropsType> = ({ course }) => {
   const completedCount = concepts.filter((cpt) => cpt.isCompleted).length;
   const perc = (completedCount / concepts.length) * 100;
 
+  __cg("perc", perc);
+
   const {
-    ids: [innerIDs],
+    ids: [ids],
   } = useGenIDsV2({
     lengths: [concepts.length],
   });
 
-  __cg("paragon", innerIDs);
-
   __cg("cpts", concepts);
 
   return (
-    <div className="w-full border-[3px] border-neutral-800 h-[50px] rounded-full relative">
+    <div
+      className="w-full border-[3px] border-neutral-800 h-[50px] rounded-full relative"
+      css={css`
+        display: grid;
+        grid-template-columns: repeat(${concepts.length}, 1fr);
+        align-items: center;
+      `}
+    >
+      {concepts.map((cpt, i, arg) => (
+        <div
+          key={ids[i]}
+          className="w-[40px] h-[40px] rounded-full overflow-hidden z-60"
+          css={css`
+            justify-self: end;
+          `}
+        >
+          <ImgLoader
+            {...{
+              src: cpt.images[0].url,
+            }}
+          />
+        </div>
+      ))}
+
       <div className="absolute inset-0 overflow-hidden rounded-full">
         <div
           className="absolute top-0 left-0 h-full bg-neutral-200"
