0495e5e60185b3cd398087b4f1f9447f4298cca3
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-07-16 19:07:03 +0300

    template_left_search_bar => chaged svg

--- apps/client/src/features/layout/components/LeftSideBar/LeftSideBar.tsx
+++ apps/client/src/features/layout/components/LeftSideBar/LeftSideBar.tsx
@@ -6,9 +6,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { getLeftSideState, leftSideSLice } from "./slices/slice";
 import BlackBg from "@/common/components/elements/BlackBg/BlackBg";
 import { easeInOut, motion } from "framer-motion";
-import { css } from "@emotion/react";
 import { useMouseOut } from "@/core/hooks/ui/useMouseOut";
-import CloseBtn from "@/common/components/buttons/CloseBtn";
+import { FaAngleDoubleLeft, FaAngleDoubleRight } from "react-icons/fa";
 
 const LeftSideBar: FC = () => {
   const sideRef = useRef<HTMLDivElement | null>(null);
@@ -19,6 +18,8 @@ const LeftSideBar: FC = () => {
     ref: sideRef,
     cb: () => dispatch(leftSideSLice.actions.setSide(false)),
   });
+
+  const Svg = leftSideState.isSide ? FaAngleDoubleLeft : FaAngleDoubleRight;
 
   return (
     <>
@@ -31,28 +32,34 @@ const LeftSideBar: FC = () => {
 
       <motion.div
         ref={sideRef}
-        className="z__left_side fixed top-[80px] left-0 h-full w-[80%] md:w-[400px] bg-[#000] border-r-[3px] border-neutral-800 -translate-x-full"
+        className="z__left_side fixed top-[80px] left-0 h-full w-full sm:w-[500px] bg-[#000] border-r-[3px] border-neutral-800 -translate-x-full"
         transition={{ duration: 0.3, ease: easeInOut }}
         animate={{
-          opacity: leftSideState.isSide ? 1 : 0.5,
-          transform: `translateX(${leftSideState.isSide ? "100%" : "0%"})`,
+          transform: `translateX(${leftSideState.isSide ? "100%" : "60px"})`,
         }}
-        css={css`
-          pointer-events: ${leftSideState.isSide ? "all" : "none"};
-        `}
       >
-        <div className="grid grid-rows-[45px_1fr] min-h-0 h-full">
+        <div className="grid grid-rows-[50px_60px_1fr] min-h-0 h-full">
           <div className="w-[100px] relative h-fit max-h-fit justify-self-end mr-1 pt-10">
-            <CloseBtn
-              {...{
-                handleClick: () =>
-                  dispatch(leftSideSLice.actions.setSide(false)),
-              }}
-            />
+            <button
+              type="button"
+              className="btn__app absolute top-0 right-0"
+              style={
+                {
+                  "--scale__up": 1.25,
+                } as React.CSSProperties
+              }
+              onClick={() =>
+                dispatch(leftSideSLice.actions.setSide(!leftSideState.isSide))
+              }
+            >
+              <Svg className="text-neutral-200 min-w-[50px] min-h-[50px]" />
+            </button>
           </div>
 
-          <div className="w-full grid grid-cols-[1fr_3px_2fr] min-h-full h-full">
-            <div className=" mt-2"></div>
+          <div className="w-full h-full tb"></div>
+
+          <div className="w-full grid grid-cols-[1fr_3px_1fr] min-h-full h-full">
+            <div className="mt-2"></div>
 
             <div className="min-h-full w-full bg-neutral-800 min-w-full"></div>
             <div className=" mb-2"></div>
