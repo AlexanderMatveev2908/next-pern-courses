d7aea5d93cc6290b22fee70d69a9f6a471b3bdb1
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-07-18 14:21:01 +0300

    template_side_search_bar => added auto-focus on open sidebar

--- apps/client/src/features/layout/components/StrategicSidebar/components/ToggleSide.tsx
+++ apps/client/src/features/layout/components/StrategicSidebar/components/ToggleSide.tsx
@@ -5,8 +5,11 @@ import type { FC } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getStrategicSliceState, strategicSlice } from "../slices/slice";
 import { FaAngleDoubleLeft, FaAngleDoubleRight } from "react-icons/fa";
+import { useFormContext } from "react-hook-form";
+import { SideSummaryFormType } from "@shared/first/paperwork/concepts/schema.summary.js";
 
 const ToggleSide: FC = () => {
+  const { setFocus } = useFormContext<SideSummaryFormType>();
   const leftSideState = useSelector(getStrategicSliceState);
 
   const Svg = leftSideState.isSide ? FaAngleDoubleLeft : FaAngleDoubleRight;
@@ -22,9 +25,14 @@ const ToggleSide: FC = () => {
             "--scale__up": 1.25,
           } as React.CSSProperties
         }
-        onClick={() =>
-          dispatch(strategicSlice.actions.setSide(!leftSideState.isSide))
-        }
+        onClick={() => {
+          dispatch(strategicSlice.actions.setSide(!leftSideState.isSide));
+          if (leftSideState.isSide) return;
+          const t = setTimeout(() => {
+            setFocus("title");
+            clearTimeout(t);
+          }, 300);
+        }}
       >
         <Svg className="text-neutral-200 min-w-[50px] min-h-[50px]" />
       </button>
