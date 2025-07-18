5ed5e9c58dc3d975fdcc8774e67e117eba04d81a
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-07-18 19:48:02 +0300

    debounce_side_logic => fix

--- apps/client/src/common/components/forms/inputs/RawFieldTxt.tsx
+++ apps/client/src/common/components/forms/inputs/RawFieldTxt.tsx
@@ -3,8 +3,9 @@
 
 import { FieldArrType, FormFieldType } from "@/common/types/uiFactory";
 import { getSomePlaceholder } from "@/core/lib/etc";
-import { forwardRef, useImperativeHandle, useRef } from "react";
+import { forwardRef, RefObject, useImperativeHandle, useRef } from "react";
 import { Control, Controller, FieldValues, Path } from "react-hook-form";
+import { JSX } from "react/jsx-runtime";
 
 type PropsType<T extends FieldValues, K extends Path<T>> = {
   el: FormFieldType<T> | FieldArrType<T, K>;
@@ -13,49 +14,53 @@ type PropsType<T extends FieldValues, K extends Path<T>> = {
   cb?: (val: T[K]) => void;
 };
 
-const RawFieldTxt = forwardRef(
-  <T extends FieldValues, K extends Path<T>>(
-    { el, control, isDisabled = false, cb }: PropsType<T, K>,
-    inputRef?: React.Ref<HTMLInputElement | null>,
-  ) => {
-    const localRef = useRef<HTMLInputElement | null>(null);
+const RawFieldTxInner = <T extends FieldValues, K extends Path<T>>(
+  { el, control, isDisabled = false, cb }: PropsType<T, K>,
+  inputRef?: React.Ref<HTMLInputElement | null>,
+) => {
+  const localRef = useRef<HTMLInputElement | null>(null);
 
-    useImperativeHandle(inputRef, () => localRef.current!, []);
+  useImperativeHandle(inputRef, () => localRef.current!, []);
 
-    return (
-      <Controller
-        name={el.name as K}
-        control={control}
-        render={({ field }) => (
-          <input
-            {...field}
-            ref={(node) => {
-              field.ref(node);
-              localRef.current = node;
-            }}
-            // name={el.name}
-            required={el.required}
-            type={el.type ?? "text"}
-            placeholder={getSomePlaceholder(el)}
-            value={field.value ?? ""}
-            onChange={(e) => {
-              const {
-                target: { value },
-              } = e;
+  return (
+    <Controller
+      name={el.name as K}
+      control={control}
+      render={({ field }) => (
+        <input
+          {...field}
+          ref={(node) => {
+            field.ref(node);
+            localRef.current = node;
+          }}
+          required={el.required}
+          type={el.type ?? "text"}
+          placeholder={getSomePlaceholder(el)}
+          value={field.value ?? ""}
+          onChange={(e) => {
+            const {
+              target: { value },
+            } = e;
 
-              field.onChange(value);
+            field.onChange(value);
 
-              if (typeof cb === "function") cb(value as T[K]);
-            }}
-            disabled={isDisabled}
-            className="el__input txt__md"
-          />
-        )}
-      />
-    );
+            if (typeof cb === "function") cb(value as T[K]);
+          }}
+          disabled={isDisabled}
+          className="el__input txt__md"
+        />
+      )}
+    />
+  );
+};
+
+const RawFieldTxt = forwardRef(RawFieldTxInner) as <
+  T extends FieldValues,
+  K extends Path<T>,
+>(
+  props: PropsType<T, K> & {
+    ref?: RefObject<HTMLInputElement | null>;
   },
-);
-
-RawFieldTxt.displayName = "RawFieldTxt";
+) => JSX.Element;
 
 export default RawFieldTxt;
